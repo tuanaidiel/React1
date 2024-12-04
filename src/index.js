87f023fc65fcb47a8fed6dfa5913b3414656e4fd
tuanaidiel <tuanaidiel@gmail.com>
2024-12-04 15:59:45 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import React1 from './react1'; // Import your react1.jsx file
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<React1 />, document.getElementById('root'));
---
+++ src/react1.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import myImage from './images/logokacchan.PNG';
+
+function React1() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <img src={myImage} alt="WOIIIIII" style={{ width: '300px', height: 'auto' }} />
+    </div>
+  );
+}
+
+export default React1;
